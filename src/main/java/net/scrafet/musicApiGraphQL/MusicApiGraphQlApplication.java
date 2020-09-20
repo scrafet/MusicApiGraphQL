4f61b734c6fe05803ae4eafafd855b3d860434ac
scrafet <scrafet@gmail.com>
2020-09-20 12:41:38 -0500

    primer commit
    base creada en initilizr, dependencias : web,JPA y mysql

---
+++ src/main/java/net/scrafet/musicApiGraphQL/MusicApiGraphQlApplication.java
@@ -0,0 +1,13 @@
+package net.scrafet.musicApiGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApiGraphQlApplication.class, args);
+	}
+
+}
